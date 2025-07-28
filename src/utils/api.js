f02748c1c08b2264cf0401f66ec20edf2d6abddc
RounitSingh <rounitsingh2002@gmail.com>
2025-07-28 18:49:02 +0530

    env added

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000', // adjust as needed
+  baseURL: import.meta.env.VITE_API_BASE_URL , // adjust as needed
   withCredentials: false, // set to true if using cookies for auth
 });
 
