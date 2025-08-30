9a5c7bbe0a095d6d7eb76bea52e34830085bc3e3
RounitSingh <rounitsingh2002@gmail.com>
2025-08-30 08:35:21 +0530

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import SessionGuard from './components/SessionGuard';
+import { Analytics } from '@vercel/analytics/react';
 
 // Create a client
 const queryClient = new QueryClient({
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')).render(
       <BrowserRouter>
         <SessionGuard>
           <App />
+           <Analytics />
         </SessionGuard>
       </BrowserRouter>
     </QueryClientProvider>
